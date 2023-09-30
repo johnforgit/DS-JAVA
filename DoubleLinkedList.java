eca65a2d34533266600565e4791b1311c76a763b
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2023-09-30 19:05:14 +0530

    uploading of files related to double linked list

---
+++ DoubleLinkedList.java
@@ -0,0 +1,39 @@
+/*
+double linked list in java
+*/
+
+import java.util.Scanner;
+
+class DoubleLinkedList extends Node {
+	static Node head = null;
+	
+	public static void append(int a) {
+			Node newNode = new Node();
+			newNode.val = a;
+			newNode.prev = null;
+			newNode.next = null;
+			if(head == null)
+				head = newNode;
+			else {
+				Node temp = head;
+				while(temp.next != null)
+					temp = temp.next;
+				temp.next = newNode;
+				newNode.prev = temp;
+			}
+	}
+	
+	public static void print() {
+		Node temp = head;
+		while(temp != null) {
+			System.out.print(temp.val);
+			if(temp.next != null)
+				System.out.print("->");
+			temp = temp.next;
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+}
